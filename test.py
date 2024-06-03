f6cbff066e5d785f6055ad860aa781d0e49ae6a0
Hanna Brinkmann <hanna_brinkmann@outlook.com>
2024-06-03 14:47:33 +0200

    visualize nb exmples per lemma

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-
+#%%
+from data_preparation import get_data
 import pandas as pd
+import seaborn as sns
+
+df = get_data()
+
+print(df)
+
+# counts = pd.DataFrame(df["lemma"].value_counts())
+# print(counts)
+# ax = sns.countplot(counts, x="lemma")
+# ax.set(xlabel='Number of examples', ylabel='Count')
+
+
 
 
 
